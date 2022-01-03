a68d9872b726518540b85f864615f442500f5067
Juan Manuel Scasso <juan@selfdecode.com>
2022-01-03 15:51:29 -0300

    improve Audio Component

--- src/components/NodeComponents/AudioComponent.js
+++ src/components/NodeComponents/AudioComponent.js
@@ -1,14 +1,20 @@
 import Rete from "rete";
-import { mean } from "lodash";
+import { max } from "lodash";
 import { numSocket } from "./Sockets";
 import PreviewControl from "../NodeControls/PreviewControl";
-import ToggleControl from "../NodeControls/ToggleControl";
 import NodeTemplate from "../Rete/NodeTemplate";
 
 class AudioComponent extends Rete.Component {
   constructor() {
     super("Audio");
     this.data.component = NodeTemplate;
+  }
+
+  builder(node) {
+    const level = new Rete.Output("level", "Level", numSocket);
+    const lows = new Rete.Output("lows", "Lows", numSocket);
+    const mids = new Rete.Output("mids", "Mids", numSocket);
+    const highs = new Rete.Output("highs", "Highs", numSocket);
 
     navigator.mediaDevices
       .getUserMedia({
@@ -21,9 +27,9 @@ class AudioComponent extends Rete.Component {
         this.audioCtx = new (window.AudioContext ||
           window.webkitAudioContext)();
         this.analyser = this.audioCtx.createAnalyser();
-        this.analyser.fftSize = 128;
+        this.analyser.fftSize = 256;
         const bufferLength = this.analyser.frequencyBinCount;
-        this.dataArray = new Uint8Array(bufferLength);
+        this.frequencyData = new Uint8Array(bufferLength);
         this.source = undefined;
 
         this.source = this.audioCtx.createMediaStreamSource(stream);
@@ -34,18 +40,12 @@ class AudioComponent extends Rete.Component {
       .catch((err) => {
         console.log("u got an error:" + err);
       });
-  }
-
-  getData() {
-    this.analyser.getByteFrequencyData(this.dataArray);
-  }
-
-  builder(node) {
-    const out = new Rete.Output("num", "Level", numSocket);
 
     let cb = function () {
-      this.getData();
-      this.editor.trigger("process");
+      if (this.analyser) {
+        this.analyser.getByteFrequencyData(this.frequencyData);
+        this.editor.trigger("process");
+      }
     };
 
     this.cb = cb.bind(this);
@@ -54,20 +54,32 @@ class AudioComponent extends Rete.Component {
     node.data.intervalId = setInterval(this.cb, 50);
 
     return node
-      .addControl(new PreviewControl(this.editor, "preview", node, true))
-      .addControl(
-        new ToggleControl(this.editor, "micAccess", node, "Mic Access")
-      )
-      .addOutput(out);
+      .addControl(new PreviewControl(this.editor, "level", node, true))
+      .addControl(new PreviewControl(this.editor, "lows", node, true))
+      .addControl(new PreviewControl(this.editor, "mids", node, true))
+      .addControl(new PreviewControl(this.editor, "highs", node, true))
+      .addOutput(level)
+      .addOutput(lows)
+      .addOutput(mids)
+      .addOutput(highs);
   }
 
   worker(node, inputs, outputs) {
-    outputs["num"] = mean(this.dataArray);
+    const lows = this.frequencyData.slice(0, 1);
+    const mids = this.frequencyData.slice(1, 13);
+    const highs = this.frequencyData.slice(13, 40);
 
-    this.editor.nodes
-      .find((n) => n.id === node.id)
-      .controls.get("preview")
-      .setValue(outputs["num"]);
+    outputs["level"] = max(this.frequencyData) / 256;
+    outputs["lows"] = max(lows) / 256;
+    outputs["mids"] = max(mids) / 256;
+    outputs["highs"] = max(highs) / 256;
+
+    ["level", "lows", "mids", "highs"].forEach((value) => {
+      this.editor.nodes
+        .find((n) => n.id === node.id)
+        .controls.get(value)
+        .setValue(outputs[value]);
+    });
   }
 }
 
