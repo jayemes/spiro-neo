6bc21d8311583e4a6b4831c081c151098534a04b
Juan Manuel Scasso <juan@selfdecode.com>
2022-01-03 11:36:36 -0300

    reformat

--- src/components/Components/App.js
+++ src/components/Components/App.js
@@ -12,111 +12,112 @@ import {presets} from "../../presets";
 import theme from "../../MUI_theme";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        window.outputs = {}
+    window.outputs = {}
 
-        const code = this.props['*'] || presets[0].code
-        // const code = null
+    const code = this.props['*'] || presets[0].code
+    // const code = null
 
-        this.state = {
-            output: null,
-            style: null,
-            showHelp: false,
-            code: code,
-            showSpinner: false,
-            outputs: {},
+    this.state = {
+      output: null,
+      style: null,
+      showHelp: false,
+      code: code,
+      showSpinner: false,
+      outputs: {},
+    }
+
+    this.handleOutput = this.handleOutput.bind(this)
+    this.toggleHelp = this.toggleHelp.bind(this)
+    this.handlePreset = this.handlePreset.bind(this)
+    this.handleSpinner = this.handleSpinner.bind(this)
+    this.clearOutputs = this.clearOutputs.bind(this)
+
+    this.drawerData = {
+      presets,
+      handler: this.handlePreset
+    }
+
+  }
+
+  handleSpinner(show) {
+    return (() => this.setState({showSpinner: show}))
+  }
+
+  handlePreset(data) {
+    this.setState({code: data.code})
+  }
+
+  toggleHelp() {
+    this.setState(prev => ({showHelp: !prev.showHelp}))
+  }
+
+  handleOutput(vector, style, delta, nops, initial, transform, id, mode = 'write') {
+
+    this.setState(previous => {
+      if (mode === 'delete') {
+        const res = {...previous.outputs}
+        delete res[id]
+
+        return {outputs: res}
+      }
+
+      return {
+        outputs: {
+          ...previous.outputs,
+          [id]: {output: vector, style, delta, nops, initial, transform}
         }
+      }
+    })
 
-        this.handleOutput = this.handleOutput.bind(this)
-        this.toggleHelp = this.toggleHelp.bind(this)
-        this.handlePreset = this.handlePreset.bind(this)
-        this.handleSpinner = this.handleSpinner.bind(this)
-        this.clearOutputs = this.clearOutputs.bind(this)
+  }
 
-        this.drawerData = {
-            presets,
-            handler: this.handlePreset
-        }
+  clearOutputs() {
+    this.setState({outputs: {}})
+  }
 
-    }
+  render() {
 
-    handleSpinner(show) {
-        return (() => this.setState({showSpinner: show}))
-    }
+    return (
+      <ThemeProvider theme={theme} >
+        <div className="App" style={{height: "100vh"}}>
 
-    handlePreset(data) {
-        this.setState({code: data.code})
-    }
+          <div className="main">
+            <ThreeDElement
+              outputs={this.state.outputs}
+              handlePoints={this.handlePoints}/>
 
-    toggleHelp() {
-        this.setState(prev => ({showHelp: !prev.showHelp}))
-    }
+            <div id="nodes-container">
+              <Nodes handleOutput={this.handleOutput}
+                     handleSpinner={this.handleSpinner}
+                     clearOutputs={this.clearOutputs}
+                     code={this.state.code}
+              />
+            </div>
 
-    handleOutput(vector, style, delta, nops, initial, transform, id, mode = 'write') {
+          </div>
 
-        this.setState(previous => {
-            if (mode === 'delete') {
-                const res = {...previous.outputs}
-                delete res[id]
+          <div className="fixed-elems">
 
-                return {outputs: res}
-            }
+            <Button variant="contained" color="secondary"
+                    onClick={this.toggleHelp}> Help
+              <div className="help-icon">
+                {/*<img src={} alt=""/>*/}?
+              </div>
+            </Button>
+            <br/>
+            <Drawer data={this.drawerData}/>
 
-            return {
-                outputs: {
-                    ...previous.outputs,
-                    [id]: {output: vector, style, delta, nops, initial, transform}
-                }
-            }
-        })
+          </div>
+          {this.state.showHelp ? <Help close={this.toggleHelp}/> : null}
 
-    }
-
-    clearOutputs() {
-        this.setState({outputs: {}})
-    }
-
-    render() {
-
-        return (
-            <ThemeProvider theme={theme}>
-                <div className="App">
-
-                    <div className="main">
-                        <ThreeDElement
-                            outputs={this.state.outputs}
-                            handlePoints={this.handlePoints}/>
-
-                        <div id="nodes-container">
-                            <Nodes handleOutput={this.handleOutput}
-                                   handleSpinner={this.handleSpinner}
-                                   clearOutputs={this.clearOutputs}
-                                   code={this.state.code}
-                            />
-                        </div>
-
-                    </div>
-
-                    <div className="fixed-elems">
-
-                        <Button variant="contained" color="secondary" onClick={this.toggleHelp}> Help
-                            <div className="help-icon">
-                                {/*<img src={} alt=""/>*/}?
-                            </div>
-                        </Button>
-                        <br/>
-                        <Drawer data={this.drawerData}/>
-
-                    </div>
-                    {this.state.showHelp ? <Help close={this.toggleHelp}/> : null}
-
-                    {this.state.showSpinner ? <Spinner/> : null}
-                </div>
-            </ ThemeProvider>
-        )
-    }
+          {this.state.showSpinner ? <Spinner/> : null}
+        </div>
+      </ ThemeProvider>
+    )
+  }
 }
 
 export default App;
--- src/components/Components/Rete.js
+++ src/components/Components/Rete.js
@@ -13,7 +13,8 @@ import NumComponent from "../NodeComponents/NumComponent";
 import AddComponent from "../NodeComponents/AddComponent";
 import AnimateComponent from "../NodeComponents/AnimateComponent";
 import AddFunctionsComponent from "../NodeComponents/AddFunctionsComponent";
-import MultiplyFunctionsComponent from "../NodeComponents/MultiplyFunctionsComponent";
+import MultiplyFunctionsComponent
+  from "../NodeComponents/MultiplyFunctionsComponent";
 import SolidColorComponent from "../NodeComponents/SolidColorComponent";
 import IndexColorComponent from "../NodeComponents/IndexColorComponent";
 import MathComponent from "../NodeComponents/MathComponent";
@@ -22,69 +23,71 @@ import OutputComponent from "../NodeComponents/OutputComponent";
 import TransformComponent from "../NodeComponents/TransformComponent";
 import ColorPickerComponent from "../NodeComponents/ColorPickerComponent";
 import Rete from "rete";
+import AudioComponent from "../NodeComponents/AudioComponent";
 
 export async function createEditor(container, outputHandler, utils) {
 
-    const components = [
-        new NumComponent(),
-        new AddComponent(),
-        new MathComponent(),
-        new OutputComponent(outputHandler),
-        new SphereComponent(),
-        new CubeComponent(),
-        new CustomComponent(),
-        new AddFunctionsComponent(),
-        new MultiplyFunctionsComponent(),
-        new AnimateComponent(),
-        new SolidColorComponent(),
-        new IndexColorComponent(),
-        new TransformComponent(),
-        new ColorPickerComponent(),
-    ]
+  const components = [
+    new NumComponent(),
+    new AddComponent(),
+    new MathComponent(),
+    new OutputComponent(outputHandler),
+    new SphereComponent(),
+    new CubeComponent(),
+    new CustomComponent(),
+    new AddFunctionsComponent(),
+    new MultiplyFunctionsComponent(),
+    new AnimateComponent(),
+    new SolidColorComponent(),
+    new IndexColorComponent(),
+    new TransformComponent(),
+    new ColorPickerComponent(),
+    new AudioComponent(),
+  ]
 
-    let editor = new Rete.NodeEditor("Spiro@0.1.0", container);
-    editor.use(ConnectionPlugin);
-    editor.use(ReactRenderPlugin);
-    editor.use(AreaPlugin)
+  let editor = new Rete.NodeEditor("Spiro@0.1.0", container);
+  editor.use(ConnectionPlugin);
+  editor.use(ReactRenderPlugin);
+  editor.use(AreaPlugin)
 
-    editor.use(ContextMenuPlugin, {
-        searchBar: false,
-        delay: 100,
-    });
+  editor.use(ContextMenuPlugin, {
+    searchBar: false,
+    delay: 100,
+  });
 
-    let engine = new Rete.Engine("Spiro@0.1.0");
+  let engine = new Rete.Engine("Spiro@0.1.0");
 
-    components.forEach(c => {
-        editor.register(c);
-        engine.register(c);
-    });
+  components.forEach(c => {
+    editor.register(c);
+    engine.register(c);
+  });
 
-    engine.on('error', ({message, data}) => {
-        console.log('Engine Error: ' + message);
-    });
+  engine.on('error', ({message, data}) => {
+    console.log('Engine Error: ' + message);
+  });
 
-    editor.on(
-        "process nodecreated noderemoved connectioncreated connectionremoved",
-        async () => {
-            await engine.abort();
-            await engine.process(editor.toJSON());
-        }
-    );
+  editor.on(
+    "process nodecreated noderemoved connectioncreated connectionremoved",
+    async () => {
+      await engine.abort();
+      await engine.process(editor.toJSON());
+    }
+  );
 
-    editor.on("noderemove", node => {
-        if (node.name === "Animate") {
-            clearInterval(node.data.intervalId)
-        } else if (node.name === "Output") {
-            delete window.outputs[node.data.outputId]
-            outputHandler(null, null, null, null, null, null, node.data.outputId, 'delete')
-        }
-    })
+  editor.on("noderemove", node => {
+    if (node.name === "Animate") {
+      clearInterval(node.data.intervalId)
+    } else if (node.name === "Output") {
+      delete window.outputs[node.data.outputId]
+      outputHandler(null, null, null, null, null, null, node.data.outputId, 'delete')
+    }
+  })
 
-    editor.trigger("process");
-    editor.view.resize();
+  editor.trigger("process");
+  editor.view.resize();
 
 
-    editor.engine = engine;
+  editor.engine = engine;
 
-    return editor
+  return editor
 }
